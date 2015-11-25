79079e57552c4fc9d1bccfaf58dfde268278079d
suhaibh <suhaib.habibullah@gmail.com>
2015-11-25 00:16:55 -0500

    Fix functional test for Links controller

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class LinksControllerTest < ActionController::TestCase
   test "should get links index" do
-  	get :index, subreddit_id: 1
+  	get :index, subreddit_id: "1"
   	assert_response :success
   end
 end
