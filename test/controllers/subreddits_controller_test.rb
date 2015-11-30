8bc0997449362c6868505c3e4ea7afca24659fe4
suhaibh <suhaib.habibullah@gmail.com>
2015-11-30 00:26:54 -0500

    test/controllers/subreddits_controller_test.rb

--- test/controllers/subreddits_controller_test.rb
+++ test/controllers/subreddits_controller_test.rb
@@ -1,18 +1,27 @@
 require 'test_helper'
 
 class SubredditsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  def setup
+    @subreddit = subreddits(:subreddit_one)
+    @user = users(:luke)
+  end
+
   test "should get new" do
+    sign_in @user
     get :new
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    get :show, id: @subreddit
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit
+    sign_in @user
+    get :edit, id: @subreddit
     assert_response :success
   end
 
