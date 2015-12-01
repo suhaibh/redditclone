425c342eef134cb261eea0c8cf96d591f79b0100
suhaibh <suhaib.habibullah@gmail.com>
2015-12-01 00:34:21 -0500

    Add functional tests for index, new, show and edit actions. Test redirects for certain actions

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,8 +1,49 @@
 require 'test_helper'
 
 class LinksControllerTest < ActionController::TestCase
-  test "should get links index" do
-  	get :index
-  	assert_response :success
-  end
+
+# Add tests for the rest, including sign in
+	def setup
+		@user = users(:luke)
+		@link_one = links(:link_one)
+		@subreddit = subreddits(:subreddit_one)
+	end
+	
+	test "should get new" do
+		sign_in @user
+		get :new, subreddit_id: @subreddit
+		assert_response :success
+	end
+
+	test "should redirect new to sign in when not signed in" do
+		get :new, subreddit_id: @subreddit
+		assert_redirected_to new_user_session_path
+	end
+
+	test "should get links index" do
+  		get :index
+  		assert_response :success
+  	end
+
+  	test "should get link" do
+  		get :show, subreddit_id: @link_one.subreddit, id: @link_one
+  		assert_response :success
+  	end
+
+  	test "should get edit" do
+  		sign_in @user
+  		get :edit, subreddit_id: @link_one.subreddit, id: @link_one
+  		assert_response :success
+  	end
+
+  	test "should render edit when passed invalid information" do
+  		sign_in @user
+  		patch :edit, subreddit_id: @link_one.subreddit, id: @link_one, link: {title: "", url: "", description: ""}
+  		assert_template :edit
+  	end
+
+  	test "should redirect edit to sign in when not signed in " do
+  		get :edit, subreddit_id: @link_one.subreddit, id: @link_one
+  		assert_redirected_to new_user_session_path
+  	end
 end
