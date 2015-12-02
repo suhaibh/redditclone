2eee82724f9a1751424ced15b75d927517f7e487
suhaibh <suhaib.habibullah@gmail.com>
2015-12-02 00:56:59 -0500

    Add integration test for user sign up and add test for sign up with and without valid information

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+	
+	test "user signup with invalid information" do
+		assert_no_difference 'User.count' do
+			post user_registration_path, user: 	{name: "",
+											 	 email: "",
+											 	 password: "",
+											 	 password_confirmation: ""}
+		end
+		assert_template user_registration_path
+	end
+
+	test "user sigunup with valid information" do
+		assert_difference 'User.count', 1 do
+			post user_registration_path, user: { name:  				"Obi Wan Kenobi",
+												 email: 				"obiwankenobi@gmail.com",
+												 password: 				"password",
+												 password_confirmation: "password" }
+		end
+		assert_redirected_to root_path
+	end
+end
