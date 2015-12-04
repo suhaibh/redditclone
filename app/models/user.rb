919932c1a004dd1e00bb3e7fd2740de1bcd94ce9
suhaibh <suhaib.habibullah@gmail.com>
2015-12-04 00:03:41 -0500

    Removed downcase method in favor of built in Devise downcase on save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  
-  before_save :downcase_name
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :links
   has_many :comments, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   
 
  # total link score for a given user
+ def email_required?
+    false
+ end
+
  def score
   	total = 0
   	links.each do |link|
@@ -21,17 +23,8 @@ class User < ActiveRecord::Base
   	total
  end
 
- def email_required?
-    false
- end
-
  def email_changed?
     false
  end
 
-
-  private
-  	def downcase_name
-  		self.name.downcase!
-  	end
 end
