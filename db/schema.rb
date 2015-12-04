74ae5d7d66f66ac7a2856603ef02aac81ea2e4c1
suhaibh <suhaib.habibullah@gmail.com>
2015-12-04 00:06:47 -0500

    Remove uniqueness index on email and add it to name attribute for users

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20150806014128) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "votes", force: :cascade do |t|
