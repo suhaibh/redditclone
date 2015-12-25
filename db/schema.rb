4f195d95a5ea54d8e869d5ad2edd7c1ac3889193
suhaibh <suhaib.habibullah@gmail.com>
2015-12-24 22:31:22 -0500

    Add user reference to subreddits table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806014128) do
+ActiveRecord::Schema.define(version: 20151225025305) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -41,7 +41,10 @@ ActiveRecord::Schema.define(version: 20150806014128) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "subreddits", ["user_id"], name: "index_subreddits_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
