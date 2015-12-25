a0a806e116bb7dc34c64993bb3eb06b032a7f685
suhaibh <suhaib.habibullah@gmail.com>
2015-12-24 22:28:39 -0500

    Add correct_user before action, destroy, edit and update actions

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -1,6 +1,7 @@
 class SubredditsController < ApplicationController
 
   before_action :authenticate_user!, except: [:show]
+  before_action :correct_user, only: [:destroy, :edit]
 
   def new
     @subreddit = Subreddit.new
@@ -25,13 +26,29 @@ class SubredditsController < ApplicationController
   end
 
   def update
+    @subreddit = Subreddit.find(params[:id])
+    if @subreddit.update(subreddit_params)
+      flash[:notice] = "Subreddit updated!"
+      redirect_to @subreddit
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @subreddit = Subreddit.find(params[:id])
+    @subreddit.destroy
+    redirect_to root_path
   end
 
   private
     def subreddit_params
       params.require(:subreddit).permit(:title, :description)
+    end
+
+    def correct_user
+      @subreddit = Subreddit.find(params[:id])
+      current_subreddit = current_user.subreddits.find_by(id: params[:id])
+      redirect_to subreddit_path(@subreddit), notice: "Not authorized to edit this subreddit" if current_subreddit.nil?
     end
 end
